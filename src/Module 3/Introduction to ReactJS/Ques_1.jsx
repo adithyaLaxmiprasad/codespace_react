2c1029c638de490b7158143f7f2afd0be6e7f3f8
Adithya L <1by22is400@bmsit.in>
2025-05-26 19:04:49 +0000

    ques1

--- src/Module 3/Introduction to ReactJS/Ques_1.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_1.jsx
@@ -1,10 +1,3 @@
-// JSX Syntax Basics
-// Description: Write basic JSX that displays "Hello, React!"
-// Use this component in App.js
-
-
-// src/Module 3/1ntroduction to ReactJS/Ques_1.jsx
-
 import React from 'react';
 
 const Ques_1 = () => {
@@ -16,3 +9,4 @@ const Ques_1 = () => {
 };
 
 export default Ques_1;
+
