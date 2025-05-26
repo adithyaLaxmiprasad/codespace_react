97252c481ee1019452801a68f917ee1d0ddb99f5
Adithya L <1by22is400@bmsit.in>
2025-05-26 19:31:46 +0000

    ques4

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -1,42 +1,16 @@
 import React from 'react';
 
-// Error boundary component
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, info) {
-    console.error("Component error:", error, info);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h2>Something went wrong.</h2>;
-    }
-    return this.props.children;
-  }
-}
-
-// Renamed from Error to SyntaxDemo to better reflect its purpose
-function SyntaxDemo() {
+function Error() {
     return (
-        <ErrorBoundary>
-            <div>
-                <h1>Unclosed tag</h1>
-                <p>Self-closing tag example <img src="example.png" /></p>
-                <ul>
-                    <li>List item 1</li>
-                    <li>List item 2</li>
-                </ul>
-            </div>
-        </ErrorBoundary>
+        <div>
+            <h1>Unclosed tag</h1>
+            <p>Self-closing tag example <img src="example.png" /></p>
+            <ul>
+                <li>List item 1</li>
+                <li>List item 2</li>
+            </ul>
+        </div>
     );
 }
 
-export default SyntaxDemo;
+export default Error;
