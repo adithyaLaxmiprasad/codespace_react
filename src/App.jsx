28b1f9dbaf8486d264b6910b761799c047f11942
Adithya L <1by22is400@bmsit.in>
2025-05-26 19:36:35 +0000

    ques6

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import Ques_1 from './Module 3/Introduction to ReactJS/Ques_1';
 import Ques_2 from './Module 3/Introduction to ReactJS/Ques_2';
 import Greeting from './Module 3/Introduction to ReactJS/Ques_3';
 import Profile from './Module 3/Introduction to ReactJS/Ques_5';
+import LoginMessage from './Module 3/Introduction to ReactJS/Ques_6';
 
 function App() {
   return (
     <div>
-      {/* Using default message */}
       <Ques_1 />
-
-      {/* Using custom message passed as a prop */}
       <Ques_1 message="Welcome to React!" />
-
-      {/* Using custom style and children */}
       <Ques_1 
         message="Styled and with children!" 
         style={{ color: 'blue', fontWeight: 'bold' }} 
@@ -21,15 +17,13 @@ function App() {
       >
         <span> - Extra content here</span>
       </Ques_1>
-
-      {/* Display current year */}
       <Ques_2 />
-      
-      {/* New Greeting component */}
       <Greeting />
-
-      {/* Profile component with props */}
       <Profile name="Amrutha" age={22} />
+
+      {/* Conditional Rendering Component */}
+      <LoginMessage isLoggedIn={true} />
+      <LoginMessage isLoggedIn={false} />
     </div>
   );
 }
--- src/Module 3/Introduction to ReactJS/Ques_6.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_6.jsx
@@ -1,6 +1,16 @@
-// Conditional Rendering
-// Description: Implement a component that renders different messages based on a passed isLoggedIn prop.
+import React from 'react';
 
-// Step 1: Write your code within the file, by the name of component as LoginMessage
-// Step 2: Modify the App.js to use LoginMessage with a boolean prop
+const LoginMessage = ({ isLoggedIn }) => {
+  return (
+    <div>
+      {isLoggedIn ? (
+        <h2>Welcome back, user!</h2>
+      ) : (
+        <h2>Please log in to continue.</h2>
+      )}
+    </div>
+  );
+};
+
+export default LoginMessage;
 
