f324335b7e62fc99b0a2cf3ebfda6c9aa32412bb
Adithya L <1by22is400@bmsit.in>
2025-05-26 19:34:25 +0000

    ques5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Ques_1 from './Module 3/Introduction to ReactJS/Ques_1';
 import Ques_2 from './Module 3/Introduction to ReactJS/Ques_2';
 import Greeting from './Module 3/Introduction to ReactJS/Ques_3';
+import Profile from './Module 3/Introduction to ReactJS/Ques_5';
 
 function App() {
   return (
@@ -26,6 +27,9 @@ function App() {
       
       {/* New Greeting component */}
       <Greeting />
+
+      {/* Profile component with props */}
+      <Profile name="Amrutha" age={22} />
     </div>
   );
 }
