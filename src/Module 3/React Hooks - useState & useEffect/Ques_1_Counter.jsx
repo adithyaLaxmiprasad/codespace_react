78bc654890fd9910589463248d135c3dd60bb1f4
Adithya L <1by22is400@bmsit.in>
2025-05-27 05:53:18 +0000

    ques1C

--- src/Module 3/React Hooks - useState & useEffect/Ques_1_Counter.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_1_Counter.jsx
@@ -1,9 +1,14 @@
-// Counter
-// 1. Goal: Create a simple counter that increments by one each time a button is clicked.
-// 2. Steps:
-//     - Initialize a state variable count with an initial value of 0 using useState .
-//     - Create a button with an onClick event that increases count by 1.
-//     - Display the current count value.
-//     - Write your code within the file, by the name of component as Counter
+import React, { useState } from 'react';
 
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
